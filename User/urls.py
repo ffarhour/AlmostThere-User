341f7c3734019a780cde9dd746a9343c5f5c9b5d
SbSpider <rajshorya@gmail.com>
2014-08-05 11:45:57 +1200

    Fixed problem with iframe

--- User/urls.py
+++ User/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 from User import views
 
 urlpatterns = [
-        url(r'^AlmostThere',views.AlmostThere, name="AlmostThere"),
+        url(r'^/AlmostThere',views.AlmostThere, name="AlmostThere"),
 		url(r'^$', views.index, name="index"),
 		]
--- UserSite/urls.py
+++ UserSite/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 import User.urls
+import User.views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'UserSite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^AlmostThere', User.views.AlmostThere, name="AlmostThere"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^.*$', include(User.urls)),
 )
