40e40c765c76ae2d15f38bcd5b27afce6be4fa55
SbSpider <rajshorya@gmail.com>
2014-08-05 16:58:34 +1200

    Got system hooked up to prediction API inside of CORE.

--- User/views.py
+++ User/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from Core.Types.Prediction import Predictor
+from Core.Types import Point
+
+from User.models import Stop, Route, Shape_Point
+
 # Create your views here.
 
 def index(request):
@@ -29,3 +34,43 @@ def ButtonNavigation(request):
 	"""
 	context = []
 	return render(request, "Navigation.html", context)
+
+def ButtonNavTimer(request):
+	"""
+	For the Timer
+	"""
+
+
+	# stopOne = Stop.objects.get(Stop_ID =  request.GET['stopOne'])
+	# stopTwo = Stop.objects.get(Stop_ID =  request.GET['stopTwo'])
+	route = Route.objects.get(Route_ID = request.GET['route'])
+
+	predictor = Predictor()
+
+	#predictor.SetCurrentLocation(Point(stopOne.Latitude, stopOne.Longitude))
+	# predictor.SetDestination(Point(stopTwo.Latitude, stopTwo.Longitude))
+
+	shape_info = Shape_Point.objects.filter(route = route)
+	points_list = []
+	for shape in shape_info:
+		points_list.append(
+				Point(
+				Latitude = shape.Latitude,
+				Longitude = shape.Longitude
+					))
+
+	predictor.SetDestination(points_list[len(points_list)-1])
+	predictor.SetPath(points_list)
+	predictor.SetCurrentPosition(points_list[0])
+	arrival_time = predictor.Calculate(30)
+
+	context = {'bus_time' : arrival_time}
+
+	return render(request, "Timer.html", context)
+
+def TimerData(request):
+	"""
+	For the TimerData
+	"""
+	context = []
+	return "Welcome"
--- UserSite/urls.py
+++ UserSite/urls.py
@@ -8,9 +8,11 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'UserSite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	
+
+    url(r'^User/TimerData/', User.views.TimerData, name="TimerData"),
     url(r'^User/ButtonNavigation/', User.views.ButtonNavigation, name="ButtonNavigation"),
     url(r'^User/ButtonOne/$', User.views.ButtonOne, name="ButtonOne"),
+    url(r'^User/ButtonNavTimer/$', User.views.ButtonNavTimer, name="ButtonNavTimer"),
     # url(r'^AlmostThere', User.views.AlmostThere, name="AlmostThere"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^User/', include('User.urls')),
