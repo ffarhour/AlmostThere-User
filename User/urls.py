a04463e2c9de51ff87b85d2b66a0e14c1521493e
SbSpider <rajshorya@gmail.com>
2014-08-08 09:39:02 +1200

    Added homepage link, so that the user can see the website upon entry.
    Configured urls so that they work better now.

--- User/urls.py
+++ User/urls.py
@@ -3,7 +3,12 @@ from django.conf.urls import patterns, include, url
 from User import views
 
 urlpatterns = patterns(
-		url(r'^ButtonOne/$', views.ButtonOne, name='ButtonOne'),
-        url(r'^AlmostThere/$',views.AlmostThere, name="AlmostThere"),
-		url(r'^$', views.index, name="index"),
+		
+    url(r'^/TimerData/$', views.TimerData, name="TimerData"),
+    url(r'^/ButtonNavigation/$', views.ButtonNavigation, name="ButtonNavigation"),
+    url(r'^/ButtonOne/$', views.ButtonOne, name="ButtonOne"),
+    url(r'^/ButtonNavTimer/$', views.ButtonNavTimer, name="ButtonNavTimer"),
+		url(r'^/ButtonOne/$', views.ButtonOne, name='ButtonOne'),
+        url(r'^/AlmostThere/$',views.AlmostThere, name="AlmostThere"),
+		url(r'^/$', views.index, name="index"),
 		)
--- UserSite/urls.py
+++ UserSite/urls.py
@@ -4,18 +4,18 @@ from django.contrib import admin
 import User.urls
 import User.views
 
+import homepage.urls
+import homepage.views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'UserSite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^User/TimerData/', User.views.TimerData, name="TimerData"),
-    url(r'^User/ButtonNavigation/', User.views.ButtonNavigation, name="ButtonNavigation"),
-    url(r'^User/ButtonOne/$', User.views.ButtonOne, name="ButtonOne"),
-    url(r'^User/ButtonNavTimer/$', User.views.ButtonNavTimer, name="ButtonNavTimer"),
     # url(r'^AlmostThere', User.views.AlmostThere, name="AlmostThere"),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^User/', include('User.urls')),
-    url(r'^$', User.views.index, name='index'),
+    url(r'^admin', include(admin.site.urls)),
+    url(r'^User', include('User.urls')),
+    url(r'homepage', include('homepage.urls')),
+    url(r'^$', homepage.views.index, name='index'),
     # url(r'^.*$', include(User.urls)),
 )
